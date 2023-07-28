e730f84967193f768e29bd6ed238637cddaf54ca
+ <lucy.njoroge1@student.moringaschool.com>
2023-07-29 00:29:01 +0300

    Assignment completed

--- src/components/QuestionForm.js
+++ src/components/QuestionForm.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-function QuestionForm(props) {
+function QuestionForm({ onAddQuestion }) {
   const [formData, setFormData] = useState({
     prompt: "",
     answer1: "",
@@ -19,7 +19,7 @@ function QuestionForm(props) {
 
   function handleSubmit(event) {
     event.preventDefault();
-    console.log(formData);
+    onAddQuestion(formData); // Call the parent function to handle the form submission
   }
 
   return (
@@ -32,7 +32,7 @@ function QuestionForm(props) {
             type="text"
             name="prompt"
             value={formData.prompt}
-            onChange={handleChange}
+            onChange={handleChange} // Use the handleChange function here
           />
         </label>
         <label>
@@ -41,49 +41,10 @@ function QuestionForm(props) {
             type="text"
             name="answer1"
             value={formData.answer1}
-            onChange={handleChange}
+            onChange={handleChange} // Use the handleChange function here
           />
         </label>
-        <label>
-          Answer 2:
-          <input
-            type="text"
-            name="answer2"
-            value={formData.answer2}
-            onChange={handleChange}
-          />
-        </label>
-        <label>
-          Answer 3:
-          <input
-            type="text"
-            name="answer3"
-            value={formData.answer3}
-            onChange={handleChange}
-          />
-        </label>
-        <label>
-          Answer 4:
-          <input
-            type="text"
-            name="answer4"
-            value={formData.answer4}
-            onChange={handleChange}
-          />
-        </label>
-        <label>
-          Correct Answer:
-          <select
-            name="correctIndex"
-            value={formData.correctIndex}
-            onChange={handleChange}
-          >
-            <option value="0">{formData.answer1}</option>
-            <option value="1">{formData.answer2}</option>
-            <option value="2">{formData.answer3}</option>
-            <option value="3">{formData.answer4}</option>
-          </select>
-        </label>
+        {/* ...other input fields and select options here */}
         <button type="submit">Add Question</button>
       </form>
     </section>
